f5bad94e4e1861b528ec5a39fdf9f76bf363a932
Juanchi2992 <juanchipicabohm2992@gmail.com>
2020-10-02 23:29:47 -0300

    Trabajo individual HTML y CSS

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {title: 'Mercado Liebre'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', function(req, res, next) {
+  res.send('Formulario recibido');
+  
+});
+
+module.exports = router;
